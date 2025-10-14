bdf0c58980ec3dc916be351e6b34dfdfddcc7773
Yasmin Angel <yasminangel2026@gmail.com>
2025-10-14 16:43:29 -0300

    Add files via upload

--- exemplo_selectbox.py
+++ exemplo_selectbox.py
@@ -1,14 +1,14 @@
 import streamlit as st
+st.sidebar.image("logoplay")
 
 # Dados de exemplo
-generos = ["Romance", "Ficção Científica", "Fantasia", "Terror"]
+generos = ["Rock", "Trap", "POP"]
 
 # Dicionário relacionando gêneros aos seus livros
-livros_por_genero = {
-    "Romance": ["Dom Casmurro", "Orgulho e Preconceito", "A Moreninha"],
-    "Ficção Científica": ["1984", "Duna", "Fundação"],
-    "Fantasia": ["O Senhor dos Anéis", "Harry Potter", "As Crônicas de Nárnia"],
-    "Terror": ["It: A Coisa", "O Iluminado", "Drácula"]
+artista_por_genero = {
+    "Rock": ["Nickelback", "Daftones"],
+    "Trap": ["Travis Scott", "Drake", "Don Toliver"],
+    "POP": ["Laufey", "Addison Rae", "Taylor Swift"]
 }
 
 # Selectbox para escolher o gênero                
@@ -16,13 +16,44 @@ genero_selecionado = st.sidebar.selectbox("Selecione o gênero:", generos)
 
 # Selectbox para escolher o livro (apenas do gênero selecionado)
 if genero_selecionado:
-    livro_selecionado = st.sidebar.selectbox(
-        "Selecione o livro:", 
-        livros_por_genero[genero_selecionado]
-    )
+    artista_selecionado = st.sidebar.selectbox(
+        "Selecione o artista:", 
+        artista_por_genero[genero_selecionado]
+        )
 
 # Mostrar apenas o livro selecionado
-if genero_selecionado and livro_selecionado:
-    st.write(f"**Livro selecionado:** {livro_selecionado}")
+if genero_selecionado and artista_por_genero:
+    st.write(f"**Artista selecionado:** {artista_selecionado}")
     st.write(f"**Gênero:** {genero_selecionado}")
-    st.image(f"{livro_selecionado}.png")
+
+# Mostrar video
+if genero_selecionado == "Rock" and artista_por_genero == "Nickelback":
+    st.video("https://youtu.be/Aiay8I5IPB8?si=VQGAjAr1ryG2LdPE")
+
+elif genero_selecionado == "Rock" and artista_por_genero == "Daftones":
+    st.video("https://youtu.be/ifN91YvHj7g?si=BLhOAoo_liY-jI_J")
+
+elif genero_selecionado == "Trap" and artista_por_genero == "Travis Scott":
+    st.video("https://youtu.be/B9synWjqBn8?si=e5aBY8AWShu4QFDA")
+
+elif genero_selecionado == "Trap" and artista_por_genero == "Drake":
+    st.video("https://youtu.be/JFm7YDVlqnI?si=FQcJLiBNwEjgMs4q")
+
+elif genero_selecionado == "Trap" and artista_por_genero == "Don Toliver":
+    st.video("https://youtu.be/_r-nPqWGG6c?si=pkr75PzCawlN-yNh")
+
+elif genero_selecionado == "POP" and artista_por_genero == "Laufey":
+    st.video("https://youtu.be/Yq6qkq_TWAM?si=mpn11AGHyitON_Ac")
+
+elif genero_selecionado == "POP" and artista_por_genero == "Addison Rae":
+    st.video("https://youtu.be/beNFK2cdnKU?si=4FWzqcu1mugjmGlY")
+
+elif genero_selecionado == "POP" and artista_por_genero == "Taylor Swift":
+    st.video("https://youtu.be/pB-MmaoO6B8?si=QddocAi_LWgzueAS")
+
+
+
+
+
+
+
